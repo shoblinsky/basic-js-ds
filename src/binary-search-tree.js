dd0611e45b44f0c585b0452ae9f11f247353fcdb
shoblinsky <timosh0307@gmail.com>
2025-11-10 22:36:52 +0500

    feat: 4 todo

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,24 +1,68 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootInt = null;
+    this.sizeInt = 0;
+  }
+
   root() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.rootInt;
   }
 
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+  add(data) {
+    if (!this.rootInt || this.sizeInt === 0) {
+      this.rootInt = new Node(data);
+      this.sizeInt += 1;
+      return;
+    }
 
-  find(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    let current = this.rootInt;
+    while (true) {
+      if (data === current.data) {
+        return;
+      }
+
+    if (data > current.data) {
+      if (!current.right) {
+        current.right = new Node(data);
+        this.sizeInt += 1;
+        return;
+      }
+      current = current.right;
+    }
+
+    if (data < current.data) {
+      if (!current.left) {
+        current.left = new Node(data);
+        this.sizeInt += 1;
+        return;
+      }
+      current = current.left;
+    }
+  }
+}
+
+  find(data) {
+    let current = this.rootInt;
+    while (current !== null) {
+
+      if (data === current.data) {
+        return current;
+      }
+
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return null;
   }
 
   has(/* data */) {
